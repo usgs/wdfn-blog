e6a4d8a248ef4f94486415497727e83af368872b
Daniel Naab <dnaab@usgs.gov>
2018-10-16 13:53:31 -0500

    Add Jenkinsfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eux -o pipefail
+
 COMMAND=$1
 ARGS=${@:2}
 
@@ -16,10 +18,7 @@ case ${COMMAND} in
         npm install
         npm run watch & cd /src && hugo server --bind=0.0.0.0 ${ARGS}
         ;;
-    bash)
-        bash -l
-        ;;
     *)
-        echo "Unknown entrypoint '${COMMAND}'. Usage: entrypoint.sh build|server|bash"
+        exec "$@"
         ;;
 esac
